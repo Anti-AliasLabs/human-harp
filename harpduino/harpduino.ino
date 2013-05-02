f1d91ba26aa5dbfd7a70573f70bfcdf2969802c3
theleadingzero <becky@antialiaslabs.com>
2013-05-02 16:30:49 +0100

    Increases and decreases rotation values

--- harpduino/harpduino.ino
+++ harpduino/harpduino.ino
@@ -21,27 +21,29 @@ int resetButton = 8;
 // other variables
 volatile int hallValue1 = -1;
 volatile int hallValue2 = -1;
+volatile int prevHallValue1 = -1;
+volatile int prevHallValue2 = -1;
 volatile int rotationCount = 0;
 
 void setup() {
   Serial.begin( 9600 ); 
-  
+
   pinMode( pinHall1, INPUT );
   pinMode( pinHall2, INPUT );
   pinMode( resetButton, INPUT );
-  
+
   attachInterrupt( 0, firstHallTripped, CHANGE );
   attachInterrupt( 1, secondHallTripped, CHANGE );
 }
 
 void loop() {
-  
+
   // if button is pressed
   if ( digitalRead( resetButton ) ) {
     // reset the values
     rotationCount = 0;
   }
-  
+
   // print via Serial the different values
   Serial.print("hall1: ");
   Serial.print( hallValue1 );
@@ -50,18 +52,52 @@ void loop() {
   Serial.print(", rotations: ");
   Serial.print( rotationCount );
   Serial.println(",");
-  
+
   delay( 200 );
 }
 
 
+// called when magnetic field changes
+// across first Hall Effect sensor
 void firstHallTripped() {
   hallValue1 = digitalRead( pinHall1 );
-  rotationCount++;
+  updateRotations();
 }
 
+// called when magnetic field changes
+// across second Hall Effect sensor
 void secondHallTripped() {
   hallValue2 = digitalRead( pinHall2 );
+  updateRotations();
 }
+
+
+void updateRotations() {
+  // DETECTING INCREASE VS DECREASE
+  /*
+  if on the first then the second
+   we are turning counterclockwise
+   and pulling out the string
+   
+   if the same change in state happens on the second one and then the first
+   we are turning clockwise
+   and winding back in the string
+   */
+
+
+  if ( prevHallValue1 == prevHallValue2 &&
+    prevHallValue1 == hallValue1 ) {
+    rotationCount--;
+  }
+
+  if ( prevHallValue1 == prevHallValue2 &&
+    prevHallValue1 == hallValue2) {
+    rotationCount++; 
+  }
+
+  prevHallValue1 = hallValue1;
+  prevHallValue2 = hallValue2;
+}
+
 
 
